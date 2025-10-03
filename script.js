87acdc58f7c7a66c4fc9b70aaf3f04fd5c1473b4
SRdeMora <samuelmadrid.text@gmail.com>
2025-10-03 21:00:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ async function handleChatMessage(message) {
     output.scrollTop = output.scrollHeight;
 
     try {
-        const response = await fetch('http://localhost:8000/chat', {
+        const response = await fetch('https://api.srmdevai.com/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ question: message }),
